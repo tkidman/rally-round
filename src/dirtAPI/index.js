76da47ed1a4d9849ce8a1cd967e2809ce6f85f60
Tom Kidman <tom.kidman@cxense.com>
2020-04-24 21:48:14 +1000

    WIP autologin

--- src/dirtAPI/index.js
+++ src/dirtAPI/index.js
@@ -2,8 +2,48 @@ const axios = require("axios");
 const debug = require("debug")("tkidman:dirt2-results:dirtAPI");
 const fs = require("fs");
 const { cachePath } = require("../shared");
+const USERNAME = process.env.DIRT_USERNAME;
+const PASSWORD = process.env.DIRT_PASSWORD;
+const USERNAME_SELECTOR = "#Email";
+const PASSWORD_SELECTOR = "#Password";
+const LOGIN_BUTTON_SELECTOR = "#login_button_container > input";
+const puppeteer = require("puppeteer");
 
 const { cookie, xsrfh } = require("../../creds");
+
+const login = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  page.on("console", msg => debug("PAGE LOG:", msg.text()));
+
+  await page.goto("https://accounts.codemasters.com/");
+  await page.waitForNavigation();
+
+  await page.click(USERNAME_SELECTOR);
+  await page.keyboard.type(USERNAME);
+
+  await page.click(PASSWORD_SELECTOR);
+  await page.keyboard.type(PASSWORD);
+
+  await page.click(LOGIN_BUTTON_SELECTOR);
+
+  // await page.goto(`https://dirtrally2.com/clubs/find-club/page/1`);
+  const cookies = await page.cookies();
+  const cookieHeader = cookies
+    .map(cookie => `${cookie.name}=${cookie.value}`)
+    .join("; ");
+  debug(cookieHeader);
+  page.on("request", request => {
+    debug(request);
+    if (request._url.includes("Search")) {
+      debug(request._headers);
+      return {
+        Cookie: request._headers.Cookie,
+        "RaceNet.XSRFH": request._headers["RaceNet.XSRFH"]
+      };
+    }
+  });
+};
 
 const fetchChampionships = async clubId => {
   const response = await axios({
@@ -69,4 +109,9 @@ const fetchEventResults = async ({
   return response.data;
 };
 
-module.exports = { fetchChampionships, fetchRecentResults, fetchEventResults };
+module.exports = {
+  fetchChampionships,
+  fetchRecentResults,
+  fetchEventResults,
+  login
+};
