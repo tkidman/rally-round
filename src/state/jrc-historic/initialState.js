273ee17151f73b7604ed0d82ed4d7ca13e1a9af1
pynklu <tomothy.aerts@gmail.com>
2020-11-23 13:23:29 +0100

    Add endurance to historic

--- src/state/jrc-historic/initialState.js
+++ src/state/jrc-historic/initialState.js
@@ -39,6 +39,42 @@ const initialState = {
           1
         ]
       }
+    },
+    endurance: {
+      divisionName: "endurance",
+      displayName: "JRC Endurance",
+      clubs: [{ clubId: "353757", championshipIds: ["331780"] }],
+      events: [],
+      points: {
+        powerStage: [],
+        overall: [
+          35,
+          32,
+          29,
+          27,
+          25,
+          23,
+          21,
+          19,
+          17,
+          15,
+          13,
+          12,
+          11,
+          10,
+          9,
+          8,
+          7,
+          6,
+          5,
+          4,
+          3,
+          2,
+          1,
+          1,
+          1
+        ]
+      }
     }
   }
 };
