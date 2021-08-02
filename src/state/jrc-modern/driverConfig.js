66b28e162c8717d296a8c85067a73c39d1fdb861
pynklu <tomothy.aerts@gmail.com>
2021-08-02 15:51:32 +0200

    Update driverConfig for jrc-modern

--- src/state/jrc-modern/driverConfig.js
+++ src/state/jrc-modern/driverConfig.js
@@ -1,3 +1,13 @@
-const driverColumns = {};
-const sheetsConfig = {};
+const { name, raceNetName } = require("../constants/driverFieldNames");
+
+const driverColumns = {
+  [name]: "Gamertag",
+  [raceNetName]: "Racenet"
+};
+
+const sheetsConfig = {
+  sheetId: "155Q1NPyMUSflBoDkdJK3SElq_NEzNaibZNalgcM7NGI",
+  tabName: "All tiers"
+};
+
 module.exports = { driverColumns, sheetsConfig };
