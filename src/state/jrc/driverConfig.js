8e433934f810d4abd3812cca71be2ae14dc2d49a
Tom Kidman <tom.kidman@gmail.com>
2022-02-13 14:32:14 +1100

    initial config for jrc11

--- src/state/jrc/driverConfig.js
+++ src/state/jrc/driverConfig.js
@@ -1,9 +1,9 @@
 const {
-  // teamId,
+  teamId,
   name,
-  // division,
+  division,
   raceNetName,
-  // car,
+  car,
   name3,
   nationality
 } = require("../constants/driverFieldNames");
@@ -12,14 +12,14 @@ const driverColumns = {
   [name]: "Driver",
   [raceNetName]: "Gamertag",
   [name3]: "Name 3",
-  // [teamId]: "Team",
-  // [division]: "Tier",
-  // [car]: "Car",
+  [teamId]: "Team",
+  [division]: "Tier",
+  [car]: "Car",
   [nationality]: "Country"
 };
 
 const sheetsConfig = {
-  sheetId: "1jN41NMOWcgHMt7erSthKVl-P2HXHEweCIMR83B8tJVs",
+  sheetId: "1OI4FMjDhm1kE3Ie4c6p-l23UXj28LSGarBVsNC6B19E",
   tabName: "All tiers"
 };
 
--- src/state/jrc/initialState.js
+++ src/state/jrc/initialState.js
@@ -1,109 +1,46 @@
+// eslint-disable-next-line no-unused-vars
+const { JRC_CALCULATIONS } = require("../../fantasy/fantasyFormulas");
+// const { privateer } = require("../../shared");
+
 const initialState = {
   pointsForDNF: false,
-  websiteName: "jrc-results",
-  // subfolderName: "worldcup",
+  // websiteName: "jrc-results",
   showLivePoints: true,
   showLivePointsDaysRemaining: 4,
-  showCarNameAsTextInResults: true,
-  hideCarColumnInStandings: false,
-  useCarAsTeam: false,
-  useNationalityAsTeam: true,
-  disableOverall: true,
-  teamPointsForPowerstage: false,
-  dropLowestScoringRoundsNumber: 1,
-  incorrectCarTimePenaltySeconds: 120,
+  // enableDnsPenalty: true,
+  // dnsPenaltyFromFirstRound: true,
+  // dropLowestScoringRoundsNumber: 1,
+  // showCarNameAsTextInResults: true,
+  // hideCarColumnInStandings: false,
+  // useCarAsTeam: false,
+  // useNationalityAsTeam: true,
+  // disableOverall: true,
+  // teamPointsForPowerstage: false,
+  // dropLowestScoringRoundsNumber: 1,
+  // incorrectCarTimePenaltySeconds: 120,
   backgroundStyle:
-    "background-image: linear-gradient(#2193b0, #6dd5ed); background-size: cover; background-repeat: no-repeat; background-attachment: fixed;",
+    "background-image: linear-gradient(to left, #2c3e50, #fd746c); background-size: cover; background-repeat: no-repeat; background-attachment: fixed;",
   divisions: {
-    worldCup: {
-      divisionName: "worldCup",
-      displayName: "JRC World Cup",
-      disableSameCarValidation: true,
-      maxDriversScoringPointsForTeam: 3,
+    jrc1: {
+      displayName: "JRC",
+      divisionName: "jrc1",
+      maxDriversScoringPointsForTeam: 2,
       clubs: [
         {
-          clubId: "397779",
-          championshipIds: ["529964"],
+          clubId: "180867",
+          championshipIds: [],
           includeNextChampionships: true
         }
       ],
       events: [],
-      manualResults: [
-        {
-          eventIndex: 0,
-          results: [
-            {
-              name: "valuson",
-              stageTime: "08:50.929",
-              totalTime: "56:09.257",
-              isDnfEntry: false
-            }
-          ]
-        },
-        {
-          eventIndex: 3,
-          results: [
-            {
-              name: "Mike Dee",
-              stageTime: "03:21.378",
-              totalTime: "40:38.292",
-              isDnfEntry: false
-            }
-          ]
-        }
-      ],
-      cars: [
-        "Ford Fiesta R5",
-        "ŠKODA Fabia R5",
-        "Mitsubishi Space Star R5",
-        "Citroën C3 R5",
-        "Volkswagen Polo GTI R5",
-        "Peugeot 208 T16 R5",
-        "Aston Martin V8 Vantage GT4",
-        "Ford Mustang GT4",
-        "Porsche 911 RGT Rally Spec",
-        "Chevrolet Camaro GT4.R",
-        "Opel Adam R2",
-        "Peugeot 208 R2"
-      ],
       points: {
         powerStage: [5, 4, 3, 2, 1],
         overall: [
-          80,
-          74,
-          69,
-          65,
-          61,
-          58,
-          55,
-          52,
-          49,
-          47,
-          45,
-          43,
-          41,
-          39,
-          37,
-          35,
-          34,
-          33,
-          32,
-          31,
           30,
-          29,
-          28,
-          27,
-          26,
-          25,
           24,
-          23,
-          22,
           21,
-          20,
           19,
-          18,
           17,
-          16,
           15,
           14,
           13,
@@ -118,61 +55,183 @@ const initialState = {
           4,
           3,
           2,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
-          1,
           1
         ]
-      }
+      },
+      cars: [
+        "SUBARU Legacy RS",
+        "SUBARU Impreza 1995",
+        "Mitsubishi Lancer Evolution VI",
+        "Ford Escort RS Cosworth"
+      ],
+      // fantasy: true,
+      outputSheetId: "1P-0CJ4rm7xBaMsan0yMcFKwDIWkqjIvYWHNrjgFDixc",
+      manualResults: []
+      // filterEntries: {
+      //   matchDivision: true
+      // },
+      // promotionRelegation: {
+      //   relegationZone: 6
+      // }
+    },
+    jrc2: {
+      displayName: "JRC2",
+      divisionName: "jrc2",
+      maxDriversScoringPointsForTeam: 2,
+      clubs: [
+        {
+          clubId: "180867",
+          championshipIds: [],
+          includeNextChampionships: true
+        }
+      ],
+      events: [],
+      points: {
+        powerStage: [5, 4, 3, 2, 1],
+        overall: [
+          30,
+          24,
+          21,
+          19,
+          17,
+          15,
+          14,
+          13,
+          12,
+          11,
+          10,
+          9,
+          8,
+          7,
+          6,
+          5,
+          4,
+          3,
+          2,
+          1
+        ]
+      },
+      cars: [
+        "Ford Focus RS Rally 2001",
+        "Peugeot 206 Rally",
+        "SUBARU Impreza (2001)",
+        "SUBARU Impreza S4 Rally"
+      ],
+      outputSheetId: "1WaBmoqfRtXO8CEGhnE2g1b93F5o2Kjh7Nx3vi13U5Tg",
+      manualResults: []
+      // filterEntries: {
+      //   matchDivision: true
+      // },
+      // promotionRelegation: {
+      //   promotionZone: 7,
+      //   relegationZone: 7
+      // }
+    },
+    jrc3: {
+      displayName: "JRC3",
+      divisionName: "jrc3",
+      maxDriversScoringPointsForTeam: 2,
+      clubs: [
+        {
+          clubId: "330674",
+          championshipIds: [],
+          includeNextChampionships: true
+        }
+      ],
+      events: [],
+      points: {
+        powerStage: [5, 4, 3, 2, 1],
+        overall: [
+          30,
+          24,
+          21,
+          19,
+          17,
+          15,
+          14,
+          13,
+          12,
+          11,
+          10,
+          9,
+          8,
+          7,
+          6,
+          5,
+          4,
+          3,
+          2,
+          1
+        ]
+      },
+      cars: [
+        "Ford Fiesta R5",
+        "ŠKODA Fabia R5",
+        "Mitsubishi Space Star R5",
+        "Citroën C3 R5",
+        "Volkswagen Polo GTI R5",
+        "Peugeot 208 T16 R5"
+      ],
+      manualResults: []
+      // promotionRelegation: {
+      //   promotionZone: 8,
+      //   relegationZone: 8
+      // }
+    },
+    jrc4: {
+      displayName: "JRC4",
+      divisionName: "jrc4",
+      maxDriversScoringPointsForTeam: 2,
+      clubs: [
+        {
+          clubId: "342117",
+          championshipIds: [],
+          includeNextChampionships: true
+        }
+      ],
+      events: [],
+      points: {
+        powerStage: [5, 4, 3, 2, 1],
+        overall: [
+          30,
+          24,
+          21,
+          19,
+          17,
+          15,
+          14,
+          13,
+          12,
+          11,
+          10,
+          9,
+          8,
+          7,
+          6,
+          5,
+          4,
+          3,
+          2,
+          1
+        ]
+      },
+      manualResults: [],
+      // promotionRelegation: {
+      //   promotionDoubleZone: 1,
+      //   promotionZone: 7,
+      //   relegationZone: 8
+      // },
+      cars: ["Opel Adam R2", "Peugeot 208 R2"]
+      // fantasy: true
     }
   }
+  // fantasy: {
+  //   calculators: JRC_CALCULATIONS,
+  //   teams: [],
+  //   driverStandings: {},
+  //   sheetId: "1ifywqh1xfyVjpUqiG1zODpXupbyHwIzVsZCmOvcHtmg"
+  // },
+  // standingsOutputSheetId: "1iLIPjB2fsi3HI5S5CgrvnkEzrdA6fT71m_9zmtL19VI"
 };
 
 module.exports = initialState;
