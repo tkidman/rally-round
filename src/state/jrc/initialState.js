4d01dd844ec619f5f78a421b64dfb2ec30844700
Tom Kidman <tom.kidman@gmail.com>
2022-11-01 13:55:00 +1100

    enable auto jrc

--- src/state/jrc/initialState.js
+++ src/state/jrc/initialState.js
@@ -1,60 +1,65 @@
 // eslint-disable-next-line no-unused-vars
 // const { privateer } = require("../../shared");
 
+// const points = {
+//   powerStage: [5, 4, 3, 2, 1],
+//   overall: [
+//     50,
+//     44,
+//     41,
+//     38,
+//     35,
+//     32,
+//     30,
+//     28,
+//     26,
+//     24,
+//     22,
+//     20,
+//     18,
+//     17,
+//     16,
+//     15,
+//     14,
+//     13,
+//     12,
+//     11,
+//     10,
+//     9,
+//     8,
+//     7,
+//     6,
+//     5,
+//     4,
+//     3,
+//     2,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1,
+//     1
+//   ]
+// };
+
 const points = {
-  powerStage: [5, 4, 3, 2, 1],
-  overall: [
-    50,
-    44,
-    41,
-    38,
-    35,
-    32,
-    30,
-    28,
-    26,
-    24,
-    22,
-    20,
-    18,
-    17,
-    16,
-    15,
-    14,
-    13,
-    12,
-    11,
-    10,
-    9,
-    8,
-    7,
-    6,
-    5,
-    4,
-    3,
-    2,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1
-  ]
+  powerStage: [],
+  overall: []
 };
 
 const initialState = {
@@ -63,7 +68,7 @@ const initialState = {
   useStandingsForHome: true,
   showLivePoints: true,
   showLivePointsDaysRemaining: 4,
-  nullTeamIsPrivateer: false,
+  nullTeamIsPrivateer: true,
   enableDnsPenalty: true,
   dnsPenaltyFromFirstRound: true,
   // showCarNameAsTextInResults: true,
@@ -76,7 +81,10 @@ const initialState = {
   sortByDropRoundPoints: true,
   // incorrectCarTimePenaltySeconds: 120,
   backgroundStyle:
-    "background-image: linear-gradient(to left, #2c3e50, #fd746c); background-size: cover; background-repeat: no-repeat; background-attachment: fixed;",
+    "background-color: #ffffff;\n" +
+    "background-image: url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 2000 1500'%3E%3Cdefs%3E%3Crect stroke='%23ffffff' stroke-width='0.2' width='1' height='1' id='s'/%3E%3Cpattern id='a' width='3' height='3' patternUnits='userSpaceOnUse' patternTransform='scale(16.1) translate(-937.89 -703.42)'%3E%3Cuse fill='%23fcfcfc' href='%23s' y='2'/%3E%3Cuse fill='%23fcfcfc' href='%23s' x='1' y='2'/%3E%3Cuse fill='%23fafafa' href='%23s' x='2' y='2'/%3E%3Cuse fill='%23fafafa' href='%23s'/%3E%3Cuse fill='%23f7f7f7' href='%23s' x='2'/%3E%3Cuse fill='%23f7f7f7' href='%23s' x='1' y='1'/%3E%3C/pattern%3E%3Cpattern id='b' width='7' height='11' patternUnits='userSpaceOnUse' patternTransform='scale(16.1) translate(-937.89 -703.42)'%3E%3Cg fill='%23f5f5f5'%3E%3Cuse href='%23s'/%3E%3Cuse href='%23s' y='5' /%3E%3Cuse href='%23s' x='1' y='10'/%3E%3Cuse href='%23s' x='2' y='1'/%3E%3Cuse href='%23s' x='2' y='4'/%3E%3Cuse href='%23s' x='3' y='8'/%3E%3Cuse href='%23s' x='4' y='3'/%3E%3Cuse href='%23s' x='4' y='7'/%3E%3Cuse href='%23s' x='5' y='2'/%3E%3Cuse href='%23s' x='5' y='6'/%3E%3Cuse href='%23s' x='6' y='9'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='h' width='5' height='13' patternUnits='userSpaceOnUse' patternTransform='scale(16.1) translate(-937.89 -703.42)'%3E%3Cg fill='%23f5f5f5'%3E%3Cuse href='%23s' y='5'/%3E%3Cuse href='%23s' y='8'/%3E%3Cuse href='%23s' x='1' y='1'/%3E%3Cuse href='%23s' x='1' y='9'/%3E%3Cuse href='%23s' x='1' y='12'/%3E%3Cuse href='%23s' x='2'/%3E%3Cuse href='%23s' x='2' y='4'/%3E%3Cuse href='%23s' x='3' y='2'/%3E%3Cuse href='%23s' x='3' y='6'/%3E%3Cuse href='%23s' x='3' y='11'/%3E%3Cuse href='%23s' x='4' y='3'/%3E%3Cuse href='%23s' x='4' y='7'/%3E%3Cuse href='%23s' x='4' y='10'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='c' width='17' height='13' patternUnits='userSpaceOnUse' patternTransform='scale(16.1) translate(-937.89 -703.42)'%3E%3Cg fill='%23f2f2f2'%3E%3Cuse href='%23s' y='11'/%3E%3Cuse href='%23s' x='2' y='9'/%3E%3Cuse href='%23s' x='5' y='12'/%3E%3Cuse href='%23s' x='9' y='4'/%3E%3Cuse href='%23s' x='12' y='1'/%3E%3Cuse href='%23s' x='16' y='6'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='d' width='19' height='17' patternUnits='userSpaceOnUse' patternTransform='scale(16.1) translate(-937.89 -703.42)'%3E%3Cg fill='%23ffffff'%3E%3Cuse href='%23s' y='9'/%3E%3Cuse href='%23s' x='16' y='5'/%3E%3Cuse href='%23s' x='14' y='2'/%3E%3Cuse href='%23s' x='11' y='11'/%3E%3Cuse href='%23s' x='6' y='14'/%3E%3C/g%3E%3Cg fill='%23efefef'%3E%3Cuse href='%23s' x='3' y='13'/%3E%3Cuse href='%23s' x='9' y='7'/%3E%3Cuse href='%23s' x='13' y='10'/%3E%3Cuse href='%23s' x='15' y='4'/%3E%3Cuse href='%23s' x='18' y='1'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='e' width='47' height='53' patternUnits='userSpaceOnUse' patternTransform='scale(16.1) translate(-937.89 -703.42)'%3E%3Cg fill='%23DCCFD0'%3E%3Cuse href='%23s' x='2' y='5'/%3E%3Cuse href='%23s' x='16' y='38'/%3E%3Cuse href='%23s' x='46' y='42'/%3E%3Cuse href='%23s' x='29' y='20'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='f' width='59' height='71' patternUnits='userSpaceOnUse' patternTransform='scale(16.1) translate(-937.89 -703.42)'%3E%3Cg fill='%23DCCFD0'%3E%3Cuse href='%23s' x='33' y='13'/%3E%3Cuse href='%23s' x='27' y='54'/%3E%3Cuse href='%23s' x='55' y='55'/%3E%3C/g%3E%3C/pattern%3E%3Cpattern id='g' width='139' height='97' patternUnits='userSpaceOnUse' patternTransform='scale(16.1) translate(-937.89 -703.42)'%3E%3Cg fill='%23DCCFD0'%3E%3Cuse href='%23s' x='11' y='8'/%3E%3Cuse href='%23s' x='51' y='13'/%3E%3Cuse href='%23s' x='17' y='73'/%3E%3Cuse href='%23s' x='99' y='57'/%3E%3C/g%3E%3C/pattern%3E%3C/defs%3E%3Crect fill='url(%23a)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23b)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23h)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23c)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23d)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23e)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23f)' width='100%25' height='100%25'/%3E%3Crect fill='url(%23g)' width='100%25' height='100%25'/%3E%3C/svg%3E\");\n" +
+    "background-attachment: fixed;\n" +
+    "background-size: cover;",
   logo: "JRC.png",
   siteTitlePrefix: "JRC",
   divisions: {
@@ -193,7 +201,41 @@ const initialState = {
       // },
       cars: ["Opel Adam R2", "Peugeot 208 R2"]
     }
-  }
+  },
+  historicalSeasonLinks: [
+    {
+      name: "JRC 12",
+      href: "/jrc-12"
+    },
+    {
+      name: "JRC 11",
+      href: "/jrc-11"
+    },
+    {
+      name: "JRC 10",
+      href: "jrc-10"
+    },
+    {
+      name: "JRC 9",
+      href: "jrc-9"
+    },
+    {
+      name: "JRC 8",
+      href: "jrc-8"
+    },
+    {
+      name: "JRC 7",
+      href: "jrc-7"
+    },
+    {
+      name: "JRC 6",
+      href: "jrc-6"
+    },
+    {
+      name: "JRC 5",
+      href: "jrc-5"
+    }
+  ]
 };
 
 module.exports = initialState;
