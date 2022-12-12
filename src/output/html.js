35099ee3f535d7e367e3722ef5780bc171aafb71
kapoow <19243948+kapoow@users.noreply.github.com>
2022-12-12 11:43:21 +0100

    Use overall results from latest event as Home
    Adds the option to display the latest events Overall results as Home (index.js).
    
    You set it up in initialState.js with
    useResultsForHome: true,

--- src/output/html.js
+++ src/output/html.js
@@ -208,7 +208,7 @@ const writeStandingsHTML = (division, type, links) => {
     `./${outputPath}/website/${division.divisionName}-${type}-standings.html`,
     out
   );
-  if (!fs.existsSync(`./${outputPath}/website/index.html`)) {
+  if (!fs.existsSync(`./${outputPath}/website/index.html`) && leagueRef.league.useStandingsForHome) {
     fs.writeFileSync(`./${outputPath}/website/index.html`, out);
   }
 };
@@ -406,6 +406,9 @@ const writeDriverResultsHTML = (event, division, links, eventIndex) => {
     `./${outputPath}/website/${division.divisionName}-${eventIndex}-driver-results.html`,
     out
   );
+  if (division.divisionName == "overall" && leagueRef.league.useResultsForHome) {
+    fs.writeFileSync(`./${outputPath}/website/index.html`, out);
+  }
 };
 
 const addLinks = (links, name, type, displayName) => {
