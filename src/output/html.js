4db0ea169fd80e52b7d4812f9765033af0359d8e
Tom Kidman <tom.kidman@gmail.com>
2022-12-13 18:18:39 +1100

    use standings for home jrc

--- src/output/html.js
+++ src/output/html.js
@@ -208,7 +208,10 @@ const writeStandingsHTML = (division, type, links) => {
     `./${outputPath}/website/${division.divisionName}-${type}-standings.html`,
     out
   );
-  if (!fs.existsSync(`./${outputPath}/website/index.html`) && leagueRef.league.useStandingsForHome) {
+  if (
+    !fs.existsSync(`./${outputPath}/website/index.html`) &&
+    leagueRef.league.useStandingsForHome
+  ) {
     fs.writeFileSync(`./${outputPath}/website/index.html`, out);
   }
 };
@@ -406,7 +409,10 @@ const writeDriverResultsHTML = (event, division, links, eventIndex) => {
     `./${outputPath}/website/${division.divisionName}-${eventIndex}-driver-results.html`,
     out
   );
-  if (division.divisionName == "overall" && leagueRef.league.useResultsForHome) {
+  if (
+    division.divisionName == "overall" &&
+    leagueRef.league.useResultsForHome
+  ) {
     fs.writeFileSync(`./${outputPath}/website/index.html`, out);
   }
 };
