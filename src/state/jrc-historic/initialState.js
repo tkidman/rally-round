6f2d493e1d3f6b9e00a19a9866b90113ad8732e0
Tom Kidman <tom.kidman@gmail.com>
2023-01-21 14:23:02 +1100

    new jrc historic season

--- src/state/jrc-historic/initialState.js
+++ src/state/jrc-historic/initialState.js
@@ -5,16 +5,18 @@ const initialState = {
   showLivePoints: true,
   showLivePointsDaysRemaining: 4,
   showCarNameAsTextInResults: true,
+  useStandingsForHome: true,
   backgroundStyle:
-    "background-image: linear-gradient(#2193b0, #6dd5ed); background-size: cover; background-repeat: no-repeat; background-attachment: fixed;",
+    "background-color: #FFFEFE;\n" +
+    "background-image: url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='557' height='557' viewBox='0 0 800 800'%3E%3Cg fill='none' stroke='%23F7DCF9' stroke-width='1'%3E%3Cpath d='M769 229L1037 260.9M927 880L731 737 520 660 309 538 40 599 295 764 126.5 879.5 40 599-197 493 102 382-31 229 126.5 79.5-69-63'/%3E%3Cpath d='M-31 229L237 261 390 382 603 493 308.5 537.5 101.5 381.5M370 905L295 764'/%3E%3Cpath d='M520 660L578 842 731 737 840 599 603 493 520 660 295 764 309 538 390 382 539 269 769 229 577.5 41.5 370 105 295 -36 126.5 79.5 237 261 102 382 40 599 -69 737 127 880'/%3E%3Cpath d='M520-140L578.5 42.5 731-63M603 493L539 269 237 261 370 105M902 382L539 269M390 382L102 382'/%3E%3Cpath d='M-222 42L126.5 79.5 370 105 539 269 577.5 41.5 927 80 769 229 902 382 603 493 731 737M295-36L577.5 41.5M578 842L295 764M40-201L127 80M102 382L-261 269'/%3E%3C/g%3E%3Cg fill='%23FFCFD9'%3E%3Ccircle cx='769' cy='229' r='5'/%3E%3Ccircle cx='539' cy='269' r='5'/%3E%3Ccircle cx='603' cy='493' r='5'/%3E%3Ccircle cx='731' cy='737' r='5'/%3E%3Ccircle cx='520' cy='660' r='5'/%3E%3Ccircle cx='309' cy='538' r='5'/%3E%3Ccircle cx='295' cy='764' r='5'/%3E%3Ccircle cx='40' cy='599' r='5'/%3E%3Ccircle cx='102' cy='382' r='5'/%3E%3Ccircle cx='127' cy='80' r='5'/%3E%3Ccircle cx='370' cy='105' r='5'/%3E%3Ccircle cx='578' cy='42' r='5'/%3E%3Ccircle cx='237' cy='261' r='5'/%3E%3Ccircle cx='390' cy='382' r='5'/%3E%3C/g%3E%3C/svg%3E\");",
   logo: "JRC.png",
   siteTitlePrefix: "JRC Historic",
   divisions: {
-    historic: {
-      divisionName: "historic",
+    historic2301: {
+      divisionName: "historic2301",
       displayName: "JRC Historic",
       disableSameCarValidation: true,
-      clubs: [{ clubId: "192069", championshipIds: ["654949"] }],
+      clubs: [{ clubId: "192069", championshipIds: ["703027"] }],
       events: [],
       points: {
         powerStage: [],
@@ -108,7 +110,57 @@ const initialState = {
       },
       manualResults: []
     }
-  }
+  },
+  historicalSeasonLinks: [
+    {
+      name: "Historic 2022-09",
+      href: "/historic/historic-12"
+    },
+    {
+      name: "Historic 2022-08",
+      href: "/historic/historic-11"
+    },
+    {
+      name: "Historic 2022-06",
+      href: "/historic/historic-10"
+    },
+    {
+      name: "Historic 2022-01",
+      href: "/historic/historic-9"
+    },
+    {
+      name: "Historic 2021-11",
+      href: "/historic/historic-8"
+    },
+    {
+      name: "Historic 2021-09",
+      href: "/historic/historic-7"
+    },
+    {
+      name: "Historic 2021-06",
+      href: "/historic/historic-6"
+    },
+    {
+      name: "Historic 2021-04",
+      href: "/historic/historic-5"
+    },
+    {
+      name: "Historic 2021-03",
+      href: "/historic/historic-4"
+    },
+    {
+      name: "Historic 2021-01",
+      href: "/historic/historic-3"
+    },
+    {
+      name: "Historic 2020-12",
+      href: "/historic/historic-2"
+    },
+    {
+      name: "Historic 2020-11",
+      href: "/historic/historic-1"
+    }
+  ]
 };
 
 module.exports = initialState;
