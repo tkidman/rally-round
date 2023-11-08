87cd023a7274316e56bca996cce762794b0e062b
kapoow <19243948+kapoow@users.noreply.github.com>
2023-11-08 18:13:35 +0100

    RSC World Cup Final

--- src/state/rsc/initialState.js
+++ src/state/rsc/initialState.js
@@ -1,34 +1,10 @@
 const rallies = [
   {
-    eventId: 60977,
-    endTime: "2023-10-16 22:00",
-    locationName: "Round 1 Paris",
-    locationFlag: "FR",
+    eventId: 62270,
+    endTime: "2023-11-14 21:00",
+    locationName: "Final London",
+    locationFlag: "GB",
     numStages: 7
-    // enduranceRoundMultiplier: 2
-  },
-  {
-    eventId: 61348,
-    endTime: "2023-10-23 22:00",
-    locationName: "Round 2 Sydney",
-    locationFlag: "AU",
-    numStages: 7
-    // enduranceRoundMultiplier: 2
-  },
-  {
-    eventId: 61680,
-    endTime: "2023-10-30 22:00",
-    locationName: "Round 3 Switzerland",
-    locationFlag: "CH",
-    numStages: 7
-    // enduranceRoundMultiplier: 2
-  },
-  {
-    eventId: 61956,
-    endTime: "2023-11-06 22:00",
-    locationName: "Round 4 Dallas",
-    locationFlag: "US",
-    numStages: 8
     // enduranceRoundMultiplier: 2
   }
 ];
@@ -65,78 +41,13 @@ const initialState = {
     { name: "WRC 1987", href: "/rsc-1987" },
     { name: "Season 6", href: "/rsc-6" },
     { name: "Old Farts", href: "/rsc-old-farts" },
-    { name: "ERC 2018", href: "/erc-2018" }
+    { name: "ERC 2018", href: "/erc-2018" },
+    { name: "World Cup Groups", href: "/rsc-world-cup" }
   ],
   divisions: {
-    rscWorldCupA: {
-      divisionName: "rscWorldCupA",
-      displayName: "RSC World Cup A",
-      disableSameCarValidation: true,
-      // enableSameCarClassValidation: true,
-      // maxDriversScoringPointsForTeam: 3,
-      filterEntries: { matchDivision: true },
-      rbr: {
-        rallies
-      },
-      manualResults: [],
-      events: [],
-      promotionRelegation: {
-        promotionZone: 2
-      },
-      points: {
-        // powerStage: [5, 4, 3, 2, 1],
-        // leg: [7, 6, 5, 4, 3, 2, 1],
-        overall: [10, 8, 5, 3, 1]
-      }
-      // cars: ["Peugeot 205 GTI"]
-    },
-    rscWorldCupB: {
-      divisionName: "rscWorldCupB",
-      displayName: "RSC World Cup B",
-      disableSameCarValidation: true,
-      // enableSameCarClassValidation: true,
-      // maxDriversScoringPointsForTeam: 3,
-      filterEntries: { matchDivision: true },
-      rbr: {
-        rallies
-      },
-      manualResults: [],
-      events: [],
-      promotionRelegation: {
-        promotionZone: 2
-      },
-      points: {
-        // powerStage: [5, 4, 3, 2, 1],
-        // leg: [7, 6, 5, 4, 3, 2, 1],
-        overall: [10, 8, 5, 3, 1]
-      }
-      // cars: ["Peugeot 205 GTI"]
-    },
-    rscWorldCupC: {
-      divisionName: "rscWorldCupC",
-      displayName: "RSC World Cup C",
-      disableSameCarValidation: true,
-      // enableSameCarClassValidation: true,
-      // maxDriversScoringPointsForTeam: 3,
-      filterEntries: { matchDivision: true },
-      rbr: {
-        rallies
-      },
-      manualResults: [],
-      events: [],
-      promotionRelegation: {
-        promotionZone: 2
-      },
-      points: {
-        // powerStage: [5, 4, 3, 2, 1],
-        // leg: [7, 6, 5, 4, 3, 2, 1],
-        overall: [10, 8, 5, 3, 1]
-      }
-      // cars: ["Peugeot 205 GTI"]
-    },
-    rscWorldCupCD: {
-      divisionName: "rscWorldCupD",
-      displayName: "RSC World Cup D",
+    rscWorldCupFinal: {
+      divisionName: "rscWorldCupFinal",
+      displayName: "RSC World Cup Final",
       disableSameCarValidation: true,
       // enableSameCarClassValidation: true,
       // maxDriversScoringPointsForTeam: 3,
