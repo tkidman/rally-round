8debc08e280db2af58144b4da8021a502d84000b
Tom Kidman <tom.kidman@gmail.com>
2024-08-03 20:35:15 +1000

    enable auto update jrc

--- runAll.sh
+++ runAll.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "running all clubs"
+CLUB=jrc timeout 120s node runner.js
 CLUB=jrc-themed timeout 120s node runner.js
 CLUB=jrc-historic timeout 120s node runner.js
 #CLUB=rsc timeout 120s node runner.js
