6bcd62d4cd2753a6d73c197194a0cba0f6c6a16e
Tom Kidman <tom.kidman@gmail.com>
2024-08-11 09:34:51 +1000

    change sorting jrc

--- src/state/jrc/initialState.js
+++ src/state/jrc/initialState.js
@@ -73,7 +73,7 @@ const initialState = {
   // disableOverall: true,
   // teamPointsForPowerstage: false,
   dropLowestScoringRoundsNumber: 1,
-  sortByDropRoundPoints: true,
+  sortByDropRoundPoints: false,
   // incorrectCarTimePenaltySeconds: 120,
   backgroundStyle:
     "background-color: #ffffff;\n" +
