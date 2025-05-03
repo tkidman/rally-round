4ef5db9d2a7fd502478f80dd4cc26f25b4ffcba5
Tom Kidman <tom.kidman@gmail.com>
2025-05-03 17:09:11 +1000

    enable jrc-rbr-rallysprint

--- runAll.sh
+++ runAll.sh
@@ -2,7 +2,7 @@
 
 echo "running all clubs"
 CLUB=jrc-themed timeout 120s node runner.js
-#CLUB=jrc-rbr-rallysprint timeout 120s node runner.js
+CLUB=jrc-rbr-rallysprint timeout 120s node runner.js
 #CLUB=jrc-rbr timeout 120s node runner.js
 #CLUB=jrc timeout 120s node runner.js
 #CLUB=jrc-historic timeout 120s node runner.js
