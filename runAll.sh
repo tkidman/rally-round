33f34a5ae4321113851c6e82d5fba1fcba41f48f
Tom Kidman <tom.kidman@gmail.com>
2025-07-16 21:38:10 +1000

    turn off clubs

--- runAll.sh
+++ runAll.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 echo "running all clubs"
-CLUB=jrc-themed timeout 120s node runner.js
-CLUB=jrc-rbr-rallysprint timeout 120s node runner.js
-CLUB=jrc-rbr timeout 120s node runner.js
+#CLUB=jrc-themed timeout 120s node runner.js
+#CLUB=jrc-rbr-rallysprint timeout 120s node runner.js
+#CLUB=jrc-rbr timeout 120s node runner.js
 CLUB=jrc timeout 120s node runner.js
 #CLUB=jrc-historic timeout 120s node runner.js
-CLUB=rsc timeout 120s node runner.js
+#CLUB=rsc timeout 120s node runner.js
 #CLUB=oor timeout 120s node runner.js
 echo "complete"
