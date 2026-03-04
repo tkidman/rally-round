48b08bf6fe8e371de5bfcffbe88dd2a3fa2ebb92
Tom Kidman <tom.kidman@gmail.com>
2026-03-04 20:01:14 +1100

    new round rsc, new season jrc-acr

--- eslint.config.js
+++ eslint.config.js
@@ -1,86 +1,89 @@
-const js = require('@eslint/js');
-const prettier = require('eslint-config-prettier');
-const pluginPrettier = require('eslint-plugin-prettier');
-const pluginJest = require('eslint-plugin-jest');
+const js = require("@eslint/js");
+const prettier = require("eslint-config-prettier");
+const pluginPrettier = require("eslint-plugin-prettier");
+const pluginJest = require("eslint-plugin-jest");
 
 module.exports = [
   // Base recommended config
   js.configs.recommended,
-  
+
   // Files to lint
   {
-    files: ['**/*.js'],
+    files: ["**/*.js"],
     ignores: [
-      'node_modules/**',
-      'src/state/editor/node_modules/**',
-      'hidden/**',
-      '**/*.html'
+      "node_modules/**",
+      "src/state/editor/node_modules/**",
+      "hidden/**",
+      "**/*.html"
     ],
-    
+
     plugins: {
       prettier: pluginPrettier,
       jest: pluginJest
     },
-    
+
     languageOptions: {
       ecmaVersion: 2018,
-      sourceType: 'module',
+      sourceType: "module",
       globals: {
         // Node.js globals
-        process: 'readonly',
-        console: 'readonly',
-        __dirname: 'readonly',
-        __filename: 'readonly',
-        module: 'readonly',
-        require: 'readonly',
-        exports: 'writable',
-        global: 'readonly',
-        Buffer: 'readonly',
-        setTimeout: 'readonly',
-        clearTimeout: 'readonly',
-        setInterval: 'readonly',
-        clearInterval: 'readonly',
-        
+        process: "readonly",
+        console: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        module: "readonly",
+        require: "readonly",
+        exports: "writable",
+        global: "readonly",
+        Buffer: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
+
         // Browser globals (for Puppeteer code)
-        document: 'readonly',
-        window: 'readonly',
-        
+        document: "readonly",
+        window: "readonly",
+
         // Jest globals
-        test: 'readonly',
-        expect: 'readonly',
-        describe: 'readonly',
-        it: 'readonly',
-        beforeEach: 'readonly',
-        afterEach: 'readonly',
-        beforeAll: 'readonly',
-        afterAll: 'readonly',
-        jest: 'readonly'
+        test: "readonly",
+        expect: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        beforeAll: "readonly",
+        afterAll: "readonly",
+        jest: "readonly"
       }
     },
-    
+
     rules: {
       // Prettier
-      'prettier/prettier': 'error',
-      
+      "prettier/prettier": "error",
+      "linebreak-style": "off",
       // Standard-style rules (equivalent to what "standard" provides)
-      'no-var': 'error',
-      'prefer-const': 'error',
-      'no-unused-vars': ['error', { 
-        args: 'none',
-        ignoreRestSiblings: true,
-        varsIgnorePattern: '^_',
-        argsIgnorePattern: '^_',
-        caughtErrors: 'none'
-      }],
-      
+      "no-var": "error",
+      "prefer-const": "error",
+      "no-unused-vars": [
+        "error",
+        {
+          args: "none",
+          ignoreRestSiblings: true,
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+          caughtErrors: "none"
+        }
+      ],
+
       // Custom rules from original config
-      'no-console': 'off',
-      
+      "no-console": "off",
+
       // Jest rules
-      ...pluginJest.configs['flat/recommended'].rules
+      ...pluginJest.configs["flat/recommended"].rules
     }
   },
-  
+
   // Prettier config (must be last to override other configs)
   prettier
 ];
--- src/state/jrc-acr/initialState.js
+++ src/state/jrc-acr/initialState.js
@@ -21,13 +21,13 @@ const initialState = {
   divisions: {
     jrcAcr: {
       divisionName: "jrcAcr",
-      displayName: "ACR Season 1",
+      displayName: "ACR Season 2",
       disableSameCarValidation: true,
       // enableSameCarClassValidation: true,
       // maxDriversScoringPointsForTeam: 2,
       // filterEntries: true,
       manual: {
-        sheetId: "16NMBpzuof5qpctJLhEckplwWj03SAZDDgSC06gpGWkg"
+        sheetId: "1hD1HV5mDAI70wRr0clcpnq3kDz3soNV9BWu55YrVfe8"
       },
       manualResults: [],
       events: [],
